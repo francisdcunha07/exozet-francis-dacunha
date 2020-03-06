ab015e8efbc1bdd5c284fda3329fc74c0e3ee291
francisdcunha07 <francis.dcunha06@gmail.com>
2020-03-06 15:35:29 +0530

    Updates

---
+++ precache-manifest.f2ce950d2ec710e008825dde36a97d5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a40d3c16166140fdca4a9843a9d15b4d",
+    "url": "/exozet-francis-dacunha/index.html"
+  },
+  {
+    "revision": "2d3d55ff8a12fa6b1a6c",
+    "url": "/exozet-francis-dacunha/static/css/main.703b074a.chunk.css"
+  },
+  {
+    "revision": "9b631a2e815443461df4",
+    "url": "/exozet-francis-dacunha/static/js/2.8eaa9a24.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/exozet-francis-dacunha/static/js/2.8eaa9a24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d3d55ff8a12fa6b1a6c",
+    "url": "/exozet-francis-dacunha/static/js/main.e65a76da.chunk.js"
+  },
+  {
+    "revision": "d9f2b4eae300fceeae33",
+    "url": "/exozet-francis-dacunha/static/js/runtime-main.525d135e.js"
+  }
+]);
